c8847d7ce5f3ef041e6e195d79302845f4a8066d
Bill Kelm <bkelm@willamette.edu>
2019-04-24 09:39:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# alma-bursar-csv
+
+Willamette University's python code for converting the alma bursar out file to a CSV file to be passed on to a campus business office.
+
+
---
+++ charges.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+import csv
+import formatter
+
+tree = ET.parse("small-charges-2019.xml")
+root = tree.getroot()
+
+ns = { 'xb': 'http://com/exlibris/urm/rep/externalsysfinesfees/xmlbeans'}
+
+# open a file for writing
+
+with open('charges.csv', mode='w') as breaklist_data:
+	breaklist_data_writer = csv.writer(breaklist_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+	for fine_list in root.findall('.//xb:userExportedFineFeesList', ns):
+
+                user = fine_list.find('xb:user', ns)
+                user_id = user.find('xb:value', ns)
+                fee_list = fine_list.find('xb:finefeeList', ns)
+                patron_name = fine_list.find('xb:patronName', ns)
+                user_fine = fee_list.findall('xb:userFineFee', ns) 
+                for fine in user_fine:
+                     composite = fine.find('xb:compositeSum', ns)
+                     sum = composite.find('xb:sum', ns)
+                     breaklist_data_writer.writerow([patron_name.text,user_id.text,sum.text])
+
+
+
+
